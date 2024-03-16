0406e95f0b15a8f9d70dd6552bceba2672c46e15
JotaScript <jotapee.s26@gmail.com>
2024-03-16 02:15:56 -0300

    feat: repo and user types

---
+++ src/types/repo.ts
@@ -0,0 +1,8 @@
+export type Repo = {
+  name: string
+  full_name: string
+  description: string
+  language: string
+  stargazers_count: number
+  html_url: string
+}
---
+++ src/types/user.ts
@@ -0,0 +1,9 @@
+export type UserData = {
+  login: string
+  name: string
+  avatar_url: string
+  followers: number
+  following: number
+  email?: string
+  bio?: string
+}
